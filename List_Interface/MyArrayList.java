e00746dbc68164243ff7ed2490f5cc98e0863533
Gityogesh23 <yogeshpatil1844@gmail.com>
2024-08-05 13:32:51 +0530

    ArrayListDemo

---
+++ List_Interface/MyArrayList.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+import java.util.List;
+public class MyArrayList {
+    public static void main(String[] args) {
+       // List<Integer>arr=new ArrayList<>();//Class ArrayList<E> //Upcasting
+       //Or 
+       ArrayList<Integer>arr=new ArrayList<>();
+        arr.add(10);
+        arr.add(12);
+        arr.add(13);
+        arr.add(14);
+        arr.add(15);
+        for(int ele:arr){
+            System.out.println("Arraylist Become: "+ele);
+        }
+    }
+}
