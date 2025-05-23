7348f1707bed51175ebacf995afd118ab02f00d5
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-05-23 23:17:00 +0530

    commit today

---
+++ FunctionalInterfaceDemo/OptionalDemo/OptionalDemo_1.java
@@ -0,0 +1,13 @@
+package InterfacesDemo.OptionalDemo;
+
+
+public class OptionalDemo_1 {
+    
+}
+
+/* 
+//Optional<T> is a Interface=>
+What is it?
+A container object that may or may not contain a non-null value. Introduced in Java 8.
+
+*/
