dfbbcc2c0611f38e2633360b13748eb44fbb7e70
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-06-19 23:19:21 +0530

    commit noe

--- Interview_QnA_Using_Streams/ToGetDateAndTime/ToGetDateAndTime.java
+++ Interview_QnA_Using_Streams/ToGetDateAndTime/ToGetDateAndTime.java
@@ -1,10 +1,11 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 //How you will get current date and Time using java 8 API
 public class ToGetDateAndTime {
     public static void main(String[] args) {
-        int LocalDate ld= LocalDate.now();
-        int LocalTime lt=LocalTime.now();
+         LocalDate ld= LocalDate.now();
+         LocalTime lt= LocalTime.now();
         System.out.println("Todays Date is: " +ld);
         System.out.println("Now The Time is :" +lt);
     }
