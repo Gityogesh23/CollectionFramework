8bad4d9f6062bb4a82f4aa767b7cb2683f20f97a
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-06-25 22:51:30 +0530

    commit

--- Interview_QnA_Using_Streams/NewOneProgram/Java_1.java
+++ Interview_QnA_Using_Streams/NewOneProgram/Java_1.java
@@ -1,5 +1,8 @@
-package Interview_QnA_Using_Streams.NewOneProgram;
+
+import java.util.Scanner;
 
 public class Java_1 {
-    
+    public static void main(String[] args) {
+        Scannerr sc=new Scanner(System.in);
+    }
 }
