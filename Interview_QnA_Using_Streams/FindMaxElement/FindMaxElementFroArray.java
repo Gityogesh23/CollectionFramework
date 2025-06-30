98c6ee8c2ec759b4fc1ccbded894b0462c3fccab
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-06-30 19:57:41 +0530

    commit changes..

--- Interview_QnA_Using_Streams/FindMaxElement/FindMaxElementFroArray.java
+++ Interview_QnA_Using_Streams/FindMaxElement/FindMaxElementFroArray.java
@@ -43,6 +43,9 @@ Important Note: If the OptionalInt is empty (meaning the original stream was emp
 System.out.println(...)
 
 
+This is a standard Java statement used to print output to the console.
+It will print the integer value obtained from Arrays.stream(a).max().getAsInt().
+
 In Summary:
 The entire line System.out.println(Arrays.stream(a).max().getAsInt()); does the following:
 Takes the array a.
