fa5caa0b6c9d54afed5000f0c852dfc64a7ff1d6
Yogesh Patil <153370220+Gityogesh23@users.noreply.github.com>
2025-08-27 19:34:00 +0530

    finding second highest element using streams
    commit of the today..to be continuee...

---
+++ Interview_QnA_Using_Streams/FindSecHighestElement/FindSecHigh.java
@@ -0,0 +1,7 @@
+import java.util.List;
+import java.util.Arrays;
+public class FindSecHigh{
+  public static void main(String[] args){
+    List<Integer>list=Arrays.asList(11,12,14,23,55,66,77,88,1000,1001);
+  }
+}
