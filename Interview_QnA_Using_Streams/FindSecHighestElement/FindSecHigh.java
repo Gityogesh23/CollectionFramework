b8c8b1a197b5206846ac6a5a8d66562bb21a5415
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-08-28 18:05:50 +0530

    commit the change

--- Interview_QnA_Using_Streams/FindSecHighestElement/FindSecHigh.java
+++ Interview_QnA_Using_Streams/FindSecHighestElement/FindSecHigh.java
@@ -1,9 +1,15 @@
 import java.util.List;
 import java.util.Arrays;
+import java.util.Comparator;
 public class FindSecHigh{
   public static void main(String[] args){
-    List<Integer>list=Arrays.asList(11,12,14,23,55,66,77,88,1000,1001);
-
+    List<Integer>list=Arrays.asList(55,66,77,88,1000,1001);
+    int SecHighestElement=list.stream()
+    .sorted(Comparator.reverseOrder())
+    .skip(1)
+    .findFirst()
+    .get();
+System.out.println("The Second Highest Element Become: "+SecHighestElement);
   
   }
 }
