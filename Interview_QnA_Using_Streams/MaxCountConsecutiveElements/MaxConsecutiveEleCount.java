b8658b84cf58c27f6ba3177f245395429e82423b
Yogesh Patil <153370220+Gityogesh23@users.noreply.github.com>
2025-08-29 19:21:41 +0530

    Finding MaxConsecutiveEleCount using streams..

---
+++ Interview_QnA_Using_Streams/MaxCountConsecutiveElements/MaxConsecutiveEleCount.java
@@ -0,0 +1,8 @@
+
+
+public class MaxConsecutiveEleCount{
+  public static void main(String[] args){
+    int[]elements={1,1,0,0,1,1,1,2,3,4};  //we want count of 1's as per our requirement for now //it may differe as well
+    
+  }
+}
