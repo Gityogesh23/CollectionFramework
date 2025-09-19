6b5a10c31cf79ffa0a11dba20101fe0a9da900f1
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-09-19 18:57:35 +0530

    commit the code

--- Interview_QnA_Using_Streams/CheckDup.java/CheckDupElement.java
+++ Interview_QnA_Using_Streams/CheckDup.java/CheckDupElement.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class CheckDupElement {
     public static void main(String[] args) {
-        int[] l1={41,43,89,62,42,58,69,73,80,100};
-        int[]l2={10,12,13};
-        boolean checkDuplicate=Arrays.stream(l2).distinct().count() < (l2.length);
+        int[] arr1={41,43,89,62,42,58,69,73,80,100};
+        int[]arr2={10,12,13};
+        boolean checkDuplicate=Arrays.stream(arr1).distinct().count() < (arr2.length);
         System.out.println(checkDuplicate);
     }
 }
