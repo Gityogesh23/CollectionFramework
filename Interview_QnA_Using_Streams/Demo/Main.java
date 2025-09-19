42fbade7c0d97aa58ee0813f02988bd22a199250
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-09-19 18:46:30 +0530

    commit the code

---
+++ Interview_QnA_Using_Streams/Demo/Main.java
@@ -0,0 +1,15 @@
+//for anagram -->
+java.util.Scanner;
+public class Main
+{
+    public static isAnagram(String str1,tring str2){
+        if(str1.isBlank() || )
+    }
+	public static void main(String[] args) {
+		System.out.println("enter the strings1");
+		String str1=sc.nextLine();
+		System.out.println("enter the strings2");
+		String str2=sc.nextLine();
+	
+	}
+}
