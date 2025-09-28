11b8c54d9d5f74965a96ba2962e828bd3c11b9e0
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-09-28 17:59:36 +0530

    commit the code

--- Interview_QnA_Using_Streams/AddTwoNumbers/AdditionOfTwoIntegers.java
+++
@@ -1,9 +0,0 @@
-import java.util.function.BiFunction;
-
-public class AdditionOfTwoIntegers {
-    public static void main(String[] args) {
-        BiFunction<Integer,Integer,Integer>add = (a,b)->a+b;
-     int sum =add.apply(10,20);
-     System.out.println("addition of two Integers " +sum);
-    }
-}
