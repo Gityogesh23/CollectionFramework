406ef275cf0f7671971b5af53bd9670c3e1c310b
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-10-24 20:27:07 +0530

    commit for today

--- StreamAPI/Demo_1_IntStream/ArrayToStream.java
+++ StreamAPI/Demo_1_IntStream/ArrayToStream.java
@@ -1,4 +1,7 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ArrayToStream{
     public static void main(String[] args) {
@@ -8,5 +11,18 @@ public class ArrayToStream{
                 .filter(n->n%2==0)  //12,14 
                 .sum();
         System.out.println("Sum of an Array elements are : "+sumOfArrayElements);
+         /*  Convert to Stream, square each element, and print
+            Stream.of(array)
+            .map(n -> n*n)   
+            .forEach(System.out::println);
+            */
+        //can also stored it instead of printing
+        
+         List<Integer>sqaureOfElements=Stream.of(array)
+            .map(n -> n*n)
+            .collect(Collectors.toList());
+         System.out.println("Square of All Elements are ->"+sqaureOfElements);//144,256,361,400 
+         
+
     }
 }
