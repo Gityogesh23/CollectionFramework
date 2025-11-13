ed50fe7edee2ae31f6fff1a07880671c372d495d
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-11-13 17:18:40 +0530

    save changes

--- StreamAPI/FuctionalInterfaceDemo/PredicateDemo_1.java
+++ StreamAPI/FuctionalInterfaceDemo/PredicateDemo_1.java
@@ -1,14 +1,14 @@
 
 // Java program to illustrate Predicate Chaining
-
+//public <T>static boolean Predicate<T>Test(T t)
 import java.util.function.Predicate;
 public class PredicateDemo_1 {
     public static void main(String[] args)
     {
-        Predicate<Integer> greaterThanTen = (i) -> i > 10;
+        Predicate<Integer> greaterThanTen = (i) -> i > 10;//
 
         // Creating predicate
-        Predicate<Integer> lowerThanTwenty = (i) -> i < 20; 
+        Predicate<Integer> lowerThanTwenty = (i) -> i < 20; //lambda exp: number_of_args -> expression
         boolean result = greaterThanTen.and(lowerThanTwenty).test(15);
         System.out.println(result);
 
