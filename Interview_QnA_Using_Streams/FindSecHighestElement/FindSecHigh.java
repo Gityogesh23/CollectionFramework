88d35db41140672ac3a8bd4a64f7cf4bd11fdef7
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-05-07 19:03:13 +0530

    commmit about List Interface.

--- Interview_QnA_Using_Streams/FindSecHighestElement/FindSecHigh.java
+++ Interview_QnA_Using_Streams/FindSecHighestElement/FindSecHigh.java
@@ -13,3 +13,23 @@ System.out.println("The Second Highest Element Become: "+SecHighestElement);
   
   }
 }
+
+/*
+List<E> ->Ordered(sequence) Collection that allows you store duplicate elements (maintain their insertion order)
+It's an interface which maintains the insertion order and extends Collection interface
+and belongs to java.util package.
+
+Collecton<E>--E->List<E>-implements--->ArraysList<E> and (LikedList<E>,Vector,Stack).
+
+# Below  implementing classes of List interface :through them we able to create an object of List Interface:
+
+ArrayList<E> : Resizable array that maintains insertion order allows duplicates and provides
+fast random access.
+
+LinkedList<E> : Implements Doubly Linked List can be used as List,Stack, or Queue.
+
+Vector<E> : Synchronized resiazable array. slower than Arraylist but thread safe.
+
+Stack<E> : extends vector follows LIFO principle for storing elements.
+
+*/
