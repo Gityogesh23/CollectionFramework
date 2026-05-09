08603cb25d42d9440a978bd31827a0d8c44bad89
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-05-09 20:37:28 +0530

    commit

--- Map_Interface/PrintNonRepeatedChars/Main.java
+++ Map_Interface/PrintNonRepeatedChars/Main.java
@@ -1,10 +1,31 @@
 
 //TO print first non-repeatitive character from string .
 //STAR: Situation,Task,Action,Result
-import java.util.Scanner;
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.Scanner;
+// import java.util.HashMap;
+// import java.util.Map;
 
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "programming";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for(char ch : str.toCharArray()) {
+            if(map.get(ch) == 1) {
+                System.out.print(ch + " ");
+            }
+        }
+    }
+}
+/*
 public class PrintNonRepeatedChar {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
@@ -31,3 +52,7 @@ public class PrintNonRepeatedChar {
         }
     }
 }
+
+ */
+//Print all non-repeatitive characters 
+
