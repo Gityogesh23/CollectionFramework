7a439058d7a28996f17349718b66d9dae930a542
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-05-17 20:27:29 +0530

    commit the HashMap noteable point.

---
+++ Map_Interface/FirstNonRepeatitiveChars/PrintNonRepeatedChar.java
@@ -0,0 +1,69 @@
+
+//TO print first non-repeatitive character from string .
+//STAR: Situation,Task,Action,Result
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrintNonRepeatedChar {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the String :");
+        String str=sc.nextLine();
+        str=str.toLowerCase();
+        Map<Character,Integer>map=new HashMap<>();
+
+        //Step 1: to find frequency of each character.
+         for( char ch: str.toCharArray()){
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)+1);
+            }else{
+                map.put(ch,1);
+            }
+         }
+
+        //  step 2:  to print first non-repeatitive character.
+        for(char ch : str.toCharArray()){
+            if(map.get(ch)==1){
+                System.out.println("First non-repeatitive character is -->"+ch);
+                break;
+            }
+        }
+    }
+}
+
+
+/* 
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "programming";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for(char ch : str.toCharArray()) {
+            if(map.get(ch) == 1) {
+                System.out.print(ch + " ");
+            }
+        }
+    }
+}
+
+ 
+ 
+
+/*
+A HashMap is a highly efficient data structure that stores data in key-value pairs.
+ It uses a hashing algorithm to compute an index (a "bucket") where the value is 
+ stored, allowing you to retrieve, insert, or delete data in constant time, 
+
+
+
+, on average
+*/
