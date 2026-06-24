d936b64ee22ec3878a5ffdb536a9f0f160fee818
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-06-24 19:00:25 +0530

    commit the added code in comment section helps to clearing doubt

--- Interview_QnA_Using_Streams/FindSecHighestElement/FindSecHigh.java
+++ Interview_QnA_Using_Streams/FindSecHighestElement/FindSecHigh.java
@@ -6,7 +6,7 @@ public class FindSecHigh{
     List<Integer>list=Arrays.asList(55,66,77,88,1000,1001,1000);
     int SecHighestElement=list.stream()
     .distinct()  //removes duplicate values e.g . one of the 1000
-    .sorted(Comparator.reverseOrder())
+    .sorted(Comparator.reverseOrder()) //modern approach which internally calls Collections.reverseOrder()
     .skip(1)
     .findFirst()
     .orElse(null);
@@ -34,4 +34,37 @@ Vector<E> : Synchronized resiazable array. slower than Arraylist but thread safe
 
 Stack<E> : extends vector follows LIFO principle for storing elements.
 
+*/
+/*
+// Java Program to Demonstrate Working of reverseOrder()
+// method of Collections class
+// To Sort an Array in Descending Order
+
+// Importing required utility classes
+import java.util.*;
+
+// Main class
+// CollectionSorting
+public class GFG {
+
+    // Main driver method
+    public static void main(String[] args)
+    {
+
+        // Creating an array to be sorted in descending
+        // order
+        Integer[] arr = { 30, 20, 40, 10 };
+
+        // Collections.sort method is sorting the
+        // elements of arr[] in descending order
+        // later on Arrays.sort() is applied to sort array
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        // Printing the sorted array on console
+        System.out.println(
+            "Array after the use of Collection.reverseOrder()"
+            + " and Arrays.sort() :\n"
+            + Arrays.toString(arr));
+    }
+}
 */
